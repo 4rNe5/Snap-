0d8a0ade18520aec451a9bec5fcdc3994eff986e
Park Jun Hyeon / 4rNe5 <me@4rne5.dev>
2024-10-31 13:35:39 +0900

    Initial Commit

---
+++ Snapppp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snapppp Watch App
+//
+//  Created by 4rNe5 on 10/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snapppp Watch App/SnappppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnappppApp.swift
+//  Snapppp Watch App
+//
+//  Created by 4rNe5 on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Snapppp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
